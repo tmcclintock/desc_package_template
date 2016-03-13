506107501dedf3339349a463f6f77913ac4449b9
Jim Chiang <jchiang@slac.stanford.edu>
2016-03-13 08:50:21 -0700

    use Mario's updated conda-based install script; change setup/setup.sh to use eups

--- {{cookiecutter.repo_name}}/setup/setup.sh
+++ {{cookiecutter.repo_name}}/setup/setup.sh
@@ -1,2 +1,12 @@
+# To set up this package using eups, source this script after setting
+# up the LSST Stack in your bash environment.  You may also need to
+# set up some Stack dependencies explicitly, e.g.,
+#
+# $ setup lsst_sims
+#
+# as well as add non-eups-managed dependencies to your PYTHONPATH,
+# PATH, etc..
+
 inst_dir=$( cd $(dirname $BASH_SOURCE)/..; pwd -P )
-export PYTHONPATH=${inst_dir}/python:${PYTHONPATH}
+eups declare {{cookiecutter.repo_name}} -r ${inst_dir} -t ${USER}
+setup {{cookiecutter.repo_name}} -t ${USER}
